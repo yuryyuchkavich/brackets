8753374b514441037baeec916bd0eb6bd705eef9
Yury Yuchkavich <Yury_Yuchkavich@epam.com>
2019-03-07 13:01:40 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    flag = true;
+    
+    for (i = 0 ; i <bracketsConfig.length; i++){
+      
+      var array1  = [] ;
+      var array2 = [] ; 
+      
+      for (j = 0 ; j < str.length ; j++) {
+        if (str[j] == bracketsConfig[i][0]) {array1.push(str[j]);}
+        if (str[j] == bracketsConfig[i][1]) {if (array1.length>0) {array1.pop(str[j])} else flag=false;}
+        
+      
+      }
+      
+    }
+ return flag;
 }
